8996dc4936053f09841b6a81b5ffb9a72dbff8dd
Home <karanvirsingh332@gmail.com>
2022-05-20 21:18:47 +0530

    this is some real hard shit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
+import dbzpic from './Dragon_ball_z_3.webp'
 import './App.css';
 
 function App() {
+  const element = <h1>Hello, world!</h1>;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+       <div className="App-header">
+         {/* <img src={logo} className="App-logo" alt="logo" />
+         <img src={dbzpic} className="App-logo" alt="logo" /> */}
+         <img src="https://i.pinimg.com/originals/1b/ce/fe/1bcefe9201469c9fbbd932e4c5ab971d.gif"/>
+         <p>
+           {/* Edit <code>src/App.js</code> and save to reload. */}
+           This is Komi San. <br />
+           She has a communication disorder.  <br></br>
+           Poor girl.  <br></br>
+         </p>
+         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https:reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+         Learn React
+         </a>
+         <a
+         href="https://www.google.com/"
+         >
+           this is a google website
+         </a>
+       </div>
+     </div>
   );
 }
+//my-app\src\Dragon_ball_z_3.webp
+// D:\wtfisthis\my-app\src\Dragon_ball_z_3.webp
+// ..\public\index.html
 
 export default App;
